20cbfa86a17e6b256074d15830dc449f4f41e525
unknown <rillee.johansson@gmail.com>
2016-10-01 00:40:23 +0200

    Add option menu

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour {
+	
+	public void LoadScene(int level)
+	{
+		SceneManager.LoadScene (level);
+	}
+
+	public void Quit() {
+		Application.Quit ();
+	}
+}
---
+++ Assets/Scripts/OptionScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class OptionScript : MonoBehaviour {
+
+	public void BackToMainMenu ()
+	{
+		SceneManager.LoadScene (0);
+	}
+}
