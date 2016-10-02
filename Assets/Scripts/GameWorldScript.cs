c911b5d8d9ddf661c6b62d1c7552b19177aa551d
Rikard Johansson <rillee.johansson@gmail.com>
2016-10-02 19:43:47 +0200

    Initial ingame menu

---
+++ Assets/Scripts/GameWorldScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameWorldScript : MonoBehaviour {
+
+	public void ToggleButtons(Button[] buttons) 
+	{
+		for (int i = 0; i < buttons.Length; i++) 
+		{
+			Button but = buttons [i];
+			but.SetActive (but.Active);
+		}
+	}
+}
