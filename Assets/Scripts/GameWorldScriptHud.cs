0b5257f9102e1b4fb4c3acb0836584f0b2d97c33
unknown <rillee.johansson@gmail.com>
2016-10-02 23:58:58 +0200

    Menu alignment fixes

---
+++ Assets/Scripts/GameWorldScriptHud.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameWorldScriptHud : MonoBehaviour {
+
+	public GameObject[] allGroups;
+
+	public void toggleButtons(GameObject buttonGroup) 
+	{
+		foreach (GameObject closeGroup in allGroups) {
+			closeGroup.SetActive (false);
+		}
+
+		bool setStatus = !buttonGroup.activeSelf;
+		buttonGroup.SetActive(setStatus);
+	}
+
+}
--- Assets/Scripts/OptionScript.cs
+++ Assets/Scripts/OptionScript.cs
@@ -1,11 +1,24 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class OptionScript : MonoBehaviour {
 
-	public void BackToMainMenu ()
-	{
+	void Update() {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			SceneManager.LoadScene (0);
+		}
+	}
+
+	public void backToMainMenu () {
 		SceneManager.LoadScene (0);
+	}
+
+	public void applyOptions(Dropdown dropdownOption) {
+		Screen.SetResolution (800,800,false);
+		SceneManager.LoadScene (0);
+		Debug.Log ("TEST");
+		//SceneManager.LoadScene (0);
 	}
 }
