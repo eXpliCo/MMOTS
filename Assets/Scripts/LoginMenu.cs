070a17f7afb773857c3e6d3add77c19bdcd62bea
unknown <rillee.johansson@gmail.com>
2016-10-03 23:22:12 +0200

    First login menu

---
+++ Assets/Scripts/LoginMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoginMenu : MonoBehaviour {
+
+	private Transform cameraTransform;
+	private Transform cameraDesiredLookAt;
+
+	private void Start()
+	{
+		cameraTransform = Camera.main.transform;
+	}
+
+	private void Update() 
+	{
+		if (cameraDesiredLookAt != null) 
+		{
+			cameraTransform.rotation = Quaternion.Slerp (cameraTransform.rotation, cameraDesiredLookAt.rotation, 3 * Time.deltaTime);
+		}
+	}
+
+	public void LookAtMenu(Transform menuTransform)
+	{
+		cameraDesiredLookAt = menuTransform;
+	}
+}
